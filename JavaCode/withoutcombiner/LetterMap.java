dc682fcbd75eee94f43a20ac3e65f2248eb10478
Mtipari <52130371+Mtipari@users.noreply.github.com>
2019-06-23 15:17:20 +0100

    Add files via upload

---
+++ JavaCode/withoutcombiner/LetterMap.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class LetterMap extends Mapper<LongWritable, Text, Text, IntWritable> {
+    public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+           String line = value.toString();
+			{
+        	String line1=line.toLowerCase();
+               char[] carr = line1.toCharArray();
+			for (char c : carr) {
+				if(Character.isLetter(c)){
+				System.out.println(c);
+				context.write(new Text(String.valueOf(c)), new IntWritable(1));
+        }
+       
+    }
+        }   
+}
+}
